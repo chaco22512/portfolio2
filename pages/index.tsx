3205fcda80d5239432269dccb3366a58e843fcf9
chaco22512 <chaco22512@gmail.com>
2023-02-25 23:07:51 +0900

    header_socialicon_framermotion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
+import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +11,22 @@ export default function Home() {
         <title>Yusuke Kaga Portfolio</title>
       </Head>
 
-      <h1 className='text-red-500'>Let's build portfolio</h1>
+      {/* <h1 className='text-red-500'>Let's build portfolio</h1> */}
+
+      {/* Header */}
+      <Header />
+
+      {/* Hero */}
+      
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contacts */}
     </div>
   )
 }
