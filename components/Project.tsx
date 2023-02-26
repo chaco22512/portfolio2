7861f67f4c7bae0174c393eac83398fa7860c8f7
chaco22512 <chaco22512@gmail.com>
2023-02-26 18:38:34 +0900

    project

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import React from 'react';
+
 
 type Props = {}
 
@@ -6,7 +8,12 @@ function Project({}: Props) {
 
   const projects =[1,2,3,4,5];
   return (
-    <div className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
+    <motion.div 
+    initial={{opacity:0}}
+    transition={{duration:1.5}}
+    whileInView={{opacity:1}}
+    
+    className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
     <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         Projects</h3>
 
@@ -15,7 +22,16 @@ function Project({}: Props) {
       {/* Project */}
       {projects.map((project, i)=>(
         <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center p-20 md:p-44 h-screen'>
-          <img src="portfoliojp.png" alt="jplifehack" className='w-36 h-36' />
+          <motion.img
+          initial={{
+            y:-300,
+            opacity:0
+          }}
+          transition={{duration:1.2}}
+          whileInView={{opacity:1, y:0}}
+          viewport={{once:true}}
+          
+          src="portfoliojp.png" alt="jplifehack" className='w-36 h-36' />
 
           <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
             <h4 className='text-4xl font-semibold text-center'>
@@ -37,7 +53,7 @@ In order to do that, I hired and built a team of video editors and engineers thr
 
     <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12'></div>
 
-    </div>
+    </motion.div>
 
   )
 }
--- components/Skill.tsx
+++ components/Skill.tsx
@@ -16,15 +16,15 @@ function Skill({directionLeft}: Props) {
       transition={{duration:1}}
       whileInView={{opacity:1, x:0}}
       src="CanvaLogo.png" alt=""
-      className='rounded-full border border-gray-500 object-cover w-16 h-16
-      xl:w-20 xl:h-20 filter group-hover:grayscale transition duration-300 ease-in-out'
+      className='rounded-full border border-gray-500 object-cover h-16 w-16 md:w-20 md:h-20
+      xl:w-24 xl:h-24 filter group-hover:grayscale transition duration-300 ease-in-out'
       />
 
       <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300
-      ease-in-out group-hover:bg-white h-16 w-16 md:w-28 md:h-28
-      xl:w-32 xl:h-32 rounded-full z-0'>
+      ease-in-out group-hover:bg-white h-16 w-16 md:w-20 md:h-20
+      xl:w-24 xl:h-24 rounded-full z-0'>
         <div className='flex items-center justify-center h-full'>
-          <p className='text-3xl font-bold text-black opacity-100'>100%</p>
+          <p className='text-2xl font-bold text-black opacity-100'>100%</p>
         </div>
       </div>
 
