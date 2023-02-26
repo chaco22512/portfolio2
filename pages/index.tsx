dc1593ed6124b02d83cc91f83081111afa95e11a
chaco22512 <chaco22512@gmail.com>
2023-02-26 20:46:05 +0900

    scroll-bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,8 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className='bg-gray-900 text-white h-screen snap-y 
-    snap-mandatory overflow-y-scroll overflow-x-hidden z-0'>
+    snap-mandatory overflow-y-scroll overflow-x-hidden z-0
+    scrollbar-track-gray-400/20 scrollbar-thumb-yellow-500/80'>
       <Head>
         <title>Yusuke Kaga Portfolio</title>
       </Head>
