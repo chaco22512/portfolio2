925299347b8fe4156384fc1af5e9f5a67d232a02
chaco22512 <chaco22512@gmail.com>
2023-02-26 10:37:44 +0900

    hero

--- components/BackgroundCircle.tsx
+++ components/BackgroundCircle.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
+import { animate, motion } from 'framer-motion'
 
 type Props = {}
 
 function BackgroundCircle({}: Props) {
   return (
-    <div className='relative flex justify-center items-center'>
+    <motion.div
+    initial={{
+      opacity:0,
+    }}
+    animate={{
+      scale:[1,2,2,3,1],
+      opacity:[.1,.2,.4,.8,.1,1.0],
+      borderRadius:["20%","20%","50%","80%","20%"],
+    }}
+    transition={{
+      duration:2.5
+    }}
+
+    className='relative flex justify-center items-center'>
       <div className='absolute border border-gray-300 rounded-full h-[200px] w-[200px] mt-52 animate-ping'></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
+      <div className='absolute border border-gray-700 rounded-full h-[300px] w-[300px] mt-52'></div>
+      <div className='absolute border border-gray-700 rounded-full h-[500px] w-[500px] mt-52'></div>
+      <div className='absolute border border-yellow-300 rounded-full h-[650px] w-[650px] mt-52 animate-pulse'></div>
+      <div className='absolute border border-gray-700 rounded-full h-[800px] w-[800px] mt-52'></div>
+    </motion.div>
   )
 }
 
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,25 +1,44 @@
 import React from 'react';
 import { useTypewriter, Cursor } from 'react-simple-typewriter'
 import BackgroundCircle from './BackgroundCircle';
+import Link from 'next/link';
 
 function Hero() {
 
-  const [text, count]= useTypewriter({
-    words:["Hi, My Name is Yusuke",
-    "I am a Developper", "I am a Designer", "I am a Creator"],
-    loop:true,
-    delaySpeed:2000,
+  const [text, count] = useTypewriter({
+    words: ["Hi, My Name is Yusuke",
+      "I am a Developper", "I am a Designer", "I am a Creator"],
+    loop: true,
+    delaySpeed: 2000,
   })
 
   return (
-    <div>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
 
       <BackgroundCircle />
+      <img
+        className='relative rounded-full h-32 w-32 mx-auto object-cover z-30'
+        src="profile.png" alt="profile" />
+      <div className='z-20'>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Startup supporter</h2>
+        <h1 className='text-5xl lg:text-6xl font-semibold scroll-px-10'>
+          <span className='mr-3'>{text}</span>
+          <Cursor cursorColor='#f7ab0a'></Cursor>
+        </h1>
 
-      <h1>
-      <span>{text}</span>
-      <Cursor cursorColor='#f7ab0a'></Cursor>
-      </h1>
+        <div className='pt-5'>
+          <Link href="#about">
+            <button className='heroButton'>About</button></Link>
+          <Link href="#experience">
+            <button className='heroButton'>Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className='heroButton'>Skills</button></Link>
+          <Link href="#projects">
+            <button className='heroButton'>Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
